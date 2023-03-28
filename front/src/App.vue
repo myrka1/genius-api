4de87a32f30bf9ff13cfe7885ae993b37e7b83e3
myrka1 <myrkag93@gmail.com>
2023-03-28 01:05:39 -0700

    created frontend again, search form

---
+++ front/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/search">Search Page</router-link> |
+      <router-link to="/about">About</router-link> 
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!--
+  color scheme
+  #FF7B54    //bright orange salmon
+  #FFB26B    //light orange salmon
+  #FFD56B    //dead yellow
+  #939B62    //sage green
+-->
+
+<style>
+#app {
+  background-color: #FFB26B;
+  min-height: 100vh;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #939B62;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #FFD56B;
+}
+
+nav a.router-link-exact-active {
+  color: #FF7B54;
+}
+</style>
---
+++ front/src/views/SearchView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="search">
+      <h1>Artist Search</h1>
+
+      <form @submit.prevent="submitForm">
+        <label for="artist">Artist Name: </label> 
+        <input type="text" id="artist" v-model="artist"/> <br>
+        <button type="submit">Submit</button>
+      </form>
+      
+    </div>
+</template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        artist: "",
+      };
+    },
+    methods: {
+      submitForm() {
+        console.log("Artist: ", this.artist)
+      },
+    },
+  };
+
+</script>
+  
